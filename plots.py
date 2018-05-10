dba0d31bd3efe2a42a5e865f35378256ef4e3d58
Jackson Walters <jacksonwalters@gmail.com>
2018-05-10 13:06:22 -0500

    done

--- plots.py
+++ plots.py
@@ -1,5 +1,51 @@
 import matplotlib.pyplot as plt
 
+#SUPREME COURT SUPPORT
+################################################################################
+
+#supreme court decisions
+plt.plot(sc_support.keys(),sc_support.values(),'ro',label='Supreme Court Supp. Vote Ratio')
+
+plt.axhline(y=.5)
+
+plt.legend()
+plt.xlabel('Time (year)')
+plt.ylabel('Support')
+plt.title('SC Support for Gay Marriage')
+plt.grid(True)
+
+min_yr=min(list(sc_support.keys())+list(gay_temp_yr_avg.keys())+list(gay_mil_yr_avg.keys()))
+plt.axis([min_yr-4, CURRENT_YEAR, 0.0, 1.1])
+
+plt.savefig('./plots/sc_support.png', bbox_inches='tight')
+plt.show()
+plt.gcf().clear()
+
+#PUBLIC OPINION SUPPORT
+################################################################################
+
+#public support for gay issues
+plt.plot(gay_temp_yr_avg.keys(),gay_temp_yr_avg.values(),'g^',label='Gay/Lesbian Thermometer')
+plt.plot(gay_mil_yr_avg.keys(),gay_mil_yr_avg.values(),'bs',label='Gays in the Military')
+
+plt.axhline(y=.5)
+
+plt.legend()
+plt.xlabel('Time (year)')
+plt.ylabel('Support')
+plt.title('SC v. PO for Gay/Lesbian Issues')
+plt.grid(True)
+
+min_yr=min(list(sc_support.keys())+list(gay_temp_yr_avg.keys())+list(gay_mil_yr_avg.keys()))
+plt.axis([min_yr-4, CURRENT_YEAR, 0.0, 1.1])
+
+plt.savefig('./plots/po_support.png', bbox_inches='tight')
+plt.show()
+plt.gcf().clear()
+
+#SUPREME COURT v. PUBLIC OPINION
+################################################################################
+
 #public support for gay issues
 plt.plot(gay_temp_yr_avg.keys(),gay_temp_yr_avg.values(),'g^',label='Gay/Lesbian Thermometer')
 plt.plot(gay_mil_yr_avg.keys(),gay_mil_yr_avg.values(),'bs',label='Gays in the Military')
@@ -17,3 +63,7 @@ plt.grid(True)
 
 min_yr=min(list(sc_support.keys())+list(gay_temp_yr_avg.keys())+list(gay_mil_yr_avg.keys()))
 plt.axis([min_yr-4, CURRENT_YEAR, 0.0, 1.1])
+
+plt.savefig('./plots/sc_v_po.png', bbox_inches='tight')
+plt.show()
+plt.gcf().clear()
