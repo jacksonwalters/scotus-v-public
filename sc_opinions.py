22b2decc06a2c024484747918c0f51a29c17da29
jacksonwalters@gmail.com <jacksonwalters@gmail.com>
2018-07-17 16:26:18 -0700

    implemented response_futures for scraping

--- sc_opinions.py
+++ sc_opinions.py
@@ -8,6 +8,8 @@ import math
 import sys
 import csv
 from bs4 import BeautifulSoup
+
+sys.path.insert(0, '/Users/jackson/Documents/GitHub/requests-futures')
 from requests_futures.sessions import FuturesSession
 
 BASE_URL = 'https://www.courtlistener.com/c/'
@@ -37,8 +39,7 @@ def get_citation_url(ind,id=None):
 
 #get opinion text given supreme court case id
 #relevant tag is 'opinion content'
-def get_opinion_text(ind,id=None):
-    response = requests.get(BASE_URL + get_citation_url(ind))
+def opinion_text(response):
     soup = BeautifulSoup(response.text)
     opinion_content = soup.select('div#opinion-content') #returns a list
     if len(opinion_content) == 0:
@@ -47,10 +48,18 @@ def get_opinion_text(ind,id=None):
         return str(opinion_content)
 
 #put all opinion text into database
-def build_database():
+def scrape_opinions():
+    num_cases = len(all_cd_df)
+    session = FuturesSession(max_workers=3)
+    #create all requests in parallel
+    futures = {ind:session.get(BASE_URL + get_citation_url(ind)) for ind in range(10)}
+    #collect all results
+    responses = {ind:futures[ind].result() for ind in range(10)}
+    return responses
+
+def write_opinion_db(result):
+    #write all responses of requests to .csv file
     with open('sc_opinions.csv', 'w') as csvfile:
         opinion_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        num_cases = len(all_cd_df)
-        for ind in range(1,5):
-            opinion_text = get_opinion_text(ind)
-            opinion_writer.writerow([ind,opinion_text])
+        for ind, response in responses.items():
+            opinion_writer.writerow([ind,opinion_text(response)])
