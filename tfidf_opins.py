f8d3cb166c50e0743e839a4f91b2f30fa7da1398
jacksonwalters@gmail.com <jacksonwalters@gmail.com>
2018-08-06 16:05:57 -0700

    starting tfidf analysis

--- tfidf_opins.py
+++ tfidf_opins.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 path = '/Users/jackson/Data/scvpo/sc_opinions.csv'
 
+#store opinion data as dataframe
 opin_df = pd.read_csv(path)
+
+#generate corpus
+corpus=list(opin_df['opinion'])
+
+#generate tfidf matrix
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0)
