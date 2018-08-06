7ddfb63108f341b9ae3374d4f2e5fb5687a612b6
jacksonwalters@gmail.com <jacksonwalters@gmail.com>
2018-08-06 16:16:34 -0700

    testing tfidf

--- tfidf_opins.py
+++ tfidf_opins.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 path = '/Users/jackson/Data/scvpo/sc_opinions.csv'
@@ -8,7 +9,15 @@ path = '/Users/jackson/Data/scvpo/sc_opinions.csv'
 opin_df = pd.read_csv(path)
 
 #generate corpus
-corpus=list(opin_df['opinion'])
+corpus=[]
+for opin in list(opin_df['opinion']):
+    try:
+        if math.isnan(opin):
+            corpus.append("")
+    except TypeError:
+        opin_text = str(opin)
+        corpus.append(opin_text)
 
 #generate tfidf matrix
 tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0)
+tfidf_matrix =  tf.fit_transform(corpus[:3])
