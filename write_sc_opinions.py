b71edb3d17535d56efc588bc04fe0e9195a73db3
jacksonwalters@gmail.com <jacksonwalters@gmail.com>
2018-08-06 14:53:49 -0700

    better ids

--- write_sc_opinions.py
+++ write_sc_opinions.py
@@ -5,23 +5,35 @@ import json
 def json_opins_to_csv():
     data_path = '/Users/jackson/Data/scvpo/scotus_opinions/'
     with open('sc_opinions.csv', 'w',newline='') as csvfile:
-        fieldnames = ['id', 'opinion']
+        fieldnames = ['id','case_name','docket','opinion']
         opin_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         opin_writer.writeheader()
 
         #each json file contributes a row to the big csv file
-        total_files = 63991
         processed = 0
+        total_files = 63991
         for file in os.listdir(data_path):
             filename = os.fsdecode(file)
             if filename.endswith(".json"):
                 with open(os.path.join(data_path, filename)) as opinion_json:
+                    #load data from json
                     data = json.load(opinion_json)
+
+                    #get relevant data
                     id = filename.strip(".json")
                     opinion_text = data['plain_text']
-                    opin_writer.writerow({'id': id, 'opinion': opinion_text})
+                    dl_url = data['download_url']
+                    docket = dl_url.split('/')[-1]
+                    abs_url = data['absolute_url']
+                    case_name = abs_url.split('/')[-1]
+
+                    #write row to csv file
+                    opin_writer.writerow({'id': id, 'case_name': case_name, 'docket': docket, 'opinion': opinion_text})
+
+                    #print status
                     processed += 1
-                    print(round(processed/total_files,2))
+                    per_complete = 100*round(processed/total_files,4)
+                    print(per_complete,"%")
                     continue
             else:
                 continue
