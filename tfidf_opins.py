90342f91c846fde6992a9622b953446ed011ba4f
jacksonwalters@gmail.com <jacksonwalters@gmail.com>
2018-08-09 14:26:19 -0700

    comments

--- tfidf_opins.py
+++ tfidf_opins.py
@@ -19,13 +19,15 @@ for opin in list(opin_df['opinion']):
         corpus.append(opin_text)
 
 #generate tfidf matrix
-max_n=1
+max_n=1 #specify maximum n-gram size (length of phrases)
 tf = TfidfVectorizer(analyzer='word', ngram_range=(1,max_n), min_df = 0)
 
 #matrix with each row corresponding to an opinion
 #and each column an n-gram with n specified above
 tfidf_matrix =  tf.fit_transform(corpus)
 print(tfidf_matrix.shape)
+
+#should write this to CSV file
 
 #extract feature names, i.e. column heads
 feature_names = tf.get_feature_names()
