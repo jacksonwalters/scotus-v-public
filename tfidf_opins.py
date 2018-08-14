9484464b4617b9d00217423ca0105414767e204a
jacksonwalters@gmail.com <jacksonwalters@gmail.com>
2018-08-14 14:02:14 +0200

    build tfidf matrix and save

--- tfidf_opins.py
+++ tfidf_opins.py
@@ -4,7 +4,7 @@ import math
 from sklearn.feature_extraction.text import TfidfVectorizer
 import scipy.sparse
 
-path = '/Users/jackson/Data/scvpo/sc_opinions.csv'
+path = '/Users/jackson/Data/scvpo/scotus_opinions.csv'
 
 #store opinion data as dataframe
 opin_df = pd.read_csv(path)
@@ -23,14 +23,14 @@ for opin in list(opin_df['opinion']):
 max_n=1 #specify maximum n-gram size (length of phrases)
 tf = TfidfVectorizer(analyzer='word', ngram_range=(1,max_n), min_df = 0)
 
-#extract feature names, i.e. column heads
-feature_names = tf.get_feature_names()
-print(feature_names[:5])
-
 #matrix with each row corresponding to an opinion
 #and each column an n-gram with n specified above
 tfidf_matrix =  tf.fit_transform(corpus)
 print(tfidf_matrix.shape)
 
+#extract feature names, i.e. column heads
+feature_names = tf.get_feature_names()
+print(feature_names)
+
 #should write this matrix to file
-#scipy.sparse.save_npz('/Users/jackson/Data/scvpo/tfidf_matrix.npz', tfidf_matrix)
+scipy.sparse.save_npz('/Users/jackson/Data/scvpo/tfidf_matrix.npz', tfidf_matrix)
