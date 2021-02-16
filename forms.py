6ae99c65c08f14e021c839171e76a26e89378e84
jacksonwalters <jacksonwalters@gmail.com>
2021-02-16 13:26:35 -0500

    merging flask components
    will slowly incorporate manual/scrap work with flask app

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SubmitField
+from wtforms.validators import Optional, Length
+
+class KeywordForm(FlaskForm):
+	word1 = StringField('word1', render_kw={"placeholder": "marriage"}, validators=[Optional(),Length(max=100,message="100 chars max")])
+	word2 = StringField('word2', render_kw={"placeholder": "lgbt"}, validators=[Optional(),Length(max=100,message="100 chars max")])
+	word3 = StringField('word3', render_kw={"placeholder": "gay"}, validators=[Optional(),Length(max=100,message="100 chars max")])
+	word4 = StringField('word4', render_kw={"placeholder": "right"}, validators=[Optional(),Length(max=100,message="100 chars max")])
+	submit = SubmitField('Search Cases')
--- scvpo.py
+++ scvpo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect
 from forms import KeywordForm
 import scipy.sparse
 import pandas as pd
