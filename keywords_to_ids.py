a9229794e6f3941dc89687d781840b2a7f959b07
jacksonwalters <jacksonwalters@gmail.com>
2021-02-17 17:56:02 -0500

    commenting
    commenting and understanding this script

--- keywords_to_ids.py
+++ keywords_to_ids.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import sys
 from example_issues import civil_rights
+from load_scotus_data import all_scdb_case_data
 
-#FIND RELEVANT CASES BASED ON KEYWORDS
-##################################################################
+#load all (legacy+modern) SCOTUS case data
+all_cd_df = all_scdb_case_data()
 
-#search for a partial match in
+#given a word, search all cases
 def search(word):
     return all_cd_df[all_cd_df['caseName'].str.contains(word,na=False)]
 
+#given a case name, return the index in the list of all cases (legacy+modern)
 def sc_find_case(name):
-    upper=str.upper(name)
-    words=upper.split()
-    key_words=[]
-    year=int(words.pop().strip("()"))
-    words=" ".join(words).split("V.")
+    upper=str.upper(name) #convert name to upper case
+    words=upper.split() #split case name into list of words
+    year=int(words.pop().strip("()")) #the last word is (YEAR)
+    words=" ".join(words).split("V.") #join the words again, but split in two on V.
 
     #should search for all words in each party name
     party1=words[0].strip()
@@ -34,5 +35,5 @@ def possible_ids():
     return {name:sc_find_case(name) for name in civil_rights().scotus_cases}
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print(possible_ids())
+    print(possible_ids())
+    print(civil_rights().case_ind)
