abf03013d660c5580fbfc2fd74fd781c6133f827
jacksonwalters <jacksonwalters@gmail.com>
2021-02-17 19:01:12 -0500

    commenting
    too many possible cases. each case in the list of case names should correspond to EXACTLY ONE case in the SCDB list/dataframe/database.

--- keywords_to_ids.py
+++ keywords_to_ids.py
@@ -6,34 +6,46 @@ from load_scotus_data import all_scdb_case_data
 #load all (legacy+modern) SCOTUS case data
 all_cd_df = all_scdb_case_data()
 
-#given a word, search all cases
-def search(word):
+#given a word, search all cases and find caseNames which contain the word
+#return as a dataframe
+def search_scdb_cases_by_name(word):
     return all_cd_df[all_cd_df['caseName'].str.contains(word,na=False)]
 
 #given a case name, return the index in the list of all cases (legacy+modern)
 def sc_find_case(name):
     upper=str.upper(name) #convert name to upper case
     words=upper.split() #split case name into list of words
-    year=int(words.pop().strip("()")) #the last word is (YEAR)
-    words=" ".join(words).split("V.") #join the words again, but split in two on V.
+    #the last word is (YEAR), e.g. DOGS V. CATS (1836)
+    year=int(words.pop().strip("()"))
+    #join the words again w/spaces between, and split in two on V.
+    #e.g. ['DOGS','V.','CATS'] --> "DOGS V. CATS" --> ['DOGS','CATS']
+    partys=" ".join(words).split("V.")
 
-    #should search for all words in each party name
+    #get both parties
     party1=words[0].strip()
     party2=words[1].strip()
 
-    df1=search(party1)
-    index1=set(list(df1.index))
+    df1=search_scdb_cases_by_name(party1) #get dataframe of cases containing party1 in name
+    index1=set(list(df1.index)) #get indices of cases within df of all cases
 
-    df2=search(party2)
-    index2=set(list(df2.index))
+    df2=search_scdb_cases_by_name(party2) #get dataframe of cases containing party2 in name
+    index2=set(list(df2.index)) #get indices of cases within df of all cases
 
-    result = sorted(list(index1.intersection(index2)))
+    #keep cases in which party appears in both
+    result = index1.intersection(index2)
 
     return result
 
-def possible_ids():
-    return {name:sc_find_case(name) for name in civil_rights().scotus_cases}
+#dict of indices for each case name, i.e. {"name of SCOTUS case":[indices in SCDB case df]}
+def possible_case_indices(names):
+    return {name:sc_find_case(name) for name in names}
 
+#given a list of case names, print indices of all cases which may be relevant
 if __name__ == "__main__":
-    print(possible_ids())
+    possible_indices = possible_case_indices(civil_rights().scotus_cases)
+    indices = set()
+    for index_set in possible_indices.values():
+        indices = indices.union(index_set)
+    print(indices)
+
     print(civil_rights().case_ind)
