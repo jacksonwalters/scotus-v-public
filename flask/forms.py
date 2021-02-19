67a29ba812d204b94af4a9b484a16531322efeb5
jacksonwalters <jacksonwalters@gmail.com>
2021-02-18 21:50:25 -0500

    removed word4, not present on front end. updated placeholder.
    placeholder should encourage user to search for 2-grams

--- flask/forms.py
+++ flask/forms.py
@@ -5,6 +5,5 @@ from wtforms.validators import Optional, Length
 class KeywordForm(FlaskForm):
 	word1 = StringField('word1', render_kw={"placeholder": "marriage"}, validators=[Optional(),Length(max=100,message="100 chars max")])
 	word2 = StringField('word2', render_kw={"placeholder": "lgbt"}, validators=[Optional(),Length(max=100,message="100 chars max")])
-	word3 = StringField('word3', render_kw={"placeholder": "gay"}, validators=[Optional(),Length(max=100,message="100 chars max")])
-	word4 = StringField('word4', render_kw={"placeholder": "right"}, validators=[Optional(),Length(max=100,message="100 chars max")])
+	word3 = StringField('word3', render_kw={"placeholder": "civil rights"}, validators=[Optional(),Length(max=100,message="100 chars max")])
 	submit = SubmitField('Search Cases')
