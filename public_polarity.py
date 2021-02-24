957e507b6a4f32ffb64cdd5dd0caf82176da8a63
jacksonwalters <jacksonwalters@gmail.com>
2021-02-24 14:16:43 -0500

    placeholders are functional
    for now, the response conversion dictionaries are just *mapping all responses to ZERO*

--- public_polarity.py
+++ public_polarity.py
@@ -2,11 +2,20 @@ from random import choice
 from search_public_opinions_by_keywords import relevant_questions_anes_df
 from load_public_data import anes_opinion_data
 from example_issues import civil_rights
+import pandas as pd
 
 #ANES code for survey year
 SURVEY_YEAR = 'VCF0004'
 #identifiers for RELEVANT QUESTIONS from ANES PO surveys
 EX_PO_REL_QUES = list(civil_rights().response_map.keys())
+
+#format entry to be float if possible
+def is_num(entry):
+    try:
+        float(entry)
+        return True
+    except ValueError:
+        return False
 
 #check if the entry is scalable
 def scalable(entry,scale=(lambda x: x)):
@@ -25,39 +34,41 @@ def norm(entry,resp_conv):
     high = max(resp_conv.values())
     low = min(resp_conv.values())
     max_mag = max(abs(high),abs(low))
-    scale=(lambda x: resp_conv[x])
+    scale=(lambda x: resp_conv[x] if x in resp_conv.keys() else 0)
     if scalable(entry,scale):
-        return scale(float(entry))/max_mag   #normalize to [-1,1]
+        return scale(float(entry))/max_mag if max_mag !=0 else 0  #normalize to [-1,1]
     else:
         return float('nan')
 
-#normalize each entry in a series
-def norm_col(col,resp_conv):
-    scale=(lambda x: resp_conv[x])
-    return [norm(entry,resp_conv) for entry in col if scalable(entry,scale)]
-
-#get dict of averages for each column by year
+#get dict?? of averages for each column by year
+#placeholder until repsonses are normalized
 def scaled_avg_by_year(q_id,rel_ans_df):
     #get column for q_id
     col = rel_ans_df[ [SURVEY_YEAR,q_id] ]
-    print(col)
     #get appropriate conversion of responses to support values
-    resp_conv=resp_convert()[q_id]
-    #compute the possible max of all responses
+    #use empty placeholder dict {"":0} for now
+    resp_conv=resp_convert(q_id)
+    #compute the maximum response from all possible
     col_max=max(resp_conv.values())
     #construct a function to use as a conversion scale
     scale=(lambda x: norm(x,resp_conv) )
+
     #scale the column of question responses
     col[q_id] = col[q_id].apply(scale)
+
     #group by year and average
     ques_yr_avg=col.groupby([SURVEY_YEAR])[q_id].mean()
 
     return ques_yr_avg
+    """
+    survey_years = set(col[SURVEY_YEAR])
+    return {year:0 for year in survey_years}
+    """
 
 #dictionary keeping track of the conversion scales for
 #for every response type for PO questions
-def resp_convert():
-    return dict()
+def resp_convert(q_id):
+    return {"":0}
 
 #given a PO question, determine whether an affirmative response would be
 #considered LIBERAL or CONSERVATIVE. this is a binary classifier which
@@ -71,17 +82,19 @@ def orientation(question):
 def public_polarity(rel_ques_df,rel_ans_df):
     #get keys for relevant questions as VCF codes
     po_rel_ques_keys = list(rel_ques_df['vcf_code'])
-    print(po_rel_ques_keys)
+
     #for each question key, avgerage the responses from that column
     po_q_avgs = [scaled_avg_by_year(q_id,rel_ans_df) for q_id in po_rel_ques_keys] #collect avg's for all Q's
-    print(po_q_avgs)
-    """
+
     po_q_avgs_df = pd.concat(po_q_avgs,axis=1) #join series into df
+
     po_avgs_df = po_q_avgs_df.mean(axis=1) #take row mean
+
     po_avgs_df = po_avgs_df.loc[ po_avgs_df.notna() ] #remove NaN's
+
     po_polarity = po_avgs_df.to_dict() #go from df to dictionary
-    """
-    return dict()
+
+    return po_polarity
 
 #run a sample test
 if __name__ == "__main__":
