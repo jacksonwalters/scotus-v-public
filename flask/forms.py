5001e3a51f8929e4674c70792554900c831654c0
jacksonwalters <jacksonwalters@gmail.com>
2021-02-25 21:11:23 -0500

    update placeholder
    SCOTUS doesn't have lgbt in its vocabulary

--- flask/forms.py
+++ flask/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import Optional, Length
 
 class KeywordForm(FlaskForm):
 	word1 = StringField('word1', render_kw={"placeholder": "marriage"}, validators=[Optional(),Length(max=100,message="100 chars max")])
-	word2 = StringField('word2', render_kw={"placeholder": "lgbt"}, validators=[Optional(),Length(max=100,message="100 chars max")])
+	word2 = StringField('word2', render_kw={"placeholder": "equality"}, validators=[Optional(),Length(max=100,message="100 chars max")])
 	word3 = StringField('word3', render_kw={"placeholder": "civil rights"}, validators=[Optional(),Length(max=100,message="100 chars max")])
 	submit = SubmitField('Search Cases')
