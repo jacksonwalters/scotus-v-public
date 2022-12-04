293231b9573a1f94c9f44947213baa5814490035
Jackson Walters <jacksonwalters@gmail.com>
2022-12-04 15:38:43 -0500

    begin loading data into the SQL database

--- load_tfidf_into_sql.py
+++ load_tfidf_into_sql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import scipy.sparse
 
-#scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
+scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
 
 #sql_password = input("Enter MySQL password: ")
 
@@ -13,18 +13,19 @@ mydb = mysql.connector.connect(
     database = "scvpo"
 )
 
-print(mydb)
-
 mycursor = mydb.cursor()
-
 mycursor.execute("SHOW DATABASES")
-
 for x in mycursor:
   print(x)
 
+num_rows = scotus_tfidf_sparse_matrix.shape[0]
+num_cols = scotus_tfidf_sparse_matrix.shape[1]
+
+mycursor.execute("INSERT INTO scvpo.tfidf_scotus_opinions (row_index,col_index,tfidf_value) \
+                VALUES ({row_index},{col_index},{tfidf_value});".format(row_index=0,col_index=0,tfidf_value=0.0))
+
+#print the values in the table tfidf_scotus_opinions
 mycursor.execute("SELECT * FROM tfidf_scotus_opinions")
-
 myresult = mycursor.fetchall()
-
 for x in myresult:
   print(x)
