919ff2d4de7940d556dfe5e863225e4afb0ab23b
Jackson Walters <jacksonwalters@gmail.com>
2022-12-04 15:56:04 -0500

    iterate through nonzero entries of sparse matrix and add to SQL database
    the SQL database is scvpo and the table is tfidf_scotus_opinions

--- load_tfidf_into_sql.py
+++ load_tfidf_into_sql.py
@@ -1,11 +1,8 @@
 import mysql.connector
 import scipy.sparse
 
-scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
-
+#Create MySQL connection object to database
 #sql_password = input("Enter MySQL password: ")
-
-# Creating connection object
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "jackson",
@@ -13,16 +10,12 @@ mydb = mysql.connector.connect(
     database = "scvpo"
 )
 
-mycursor = mydb.cursor()
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-  print(x)
-
-num_rows = scotus_tfidf_sparse_matrix.shape[0]
-num_cols = scotus_tfidf_sparse_matrix.shape[1]
-
-mycursor.execute("INSERT INTO scvpo.tfidf_scotus_opinions (row_index,col_index,tfidf_value) \
-                VALUES ({row_index},{col_index},{tfidf_value});".format(row_index=0,col_index=0,tfidf_value=0.0))
+#insert sparse matrix in sql database
+scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
+rows,cols = scotus_tfidf_sparse_matrix.nonzero()
+for row,col in zip(rows,cols):
+    mycursor.execute("INSERT INTO scvpo.tfidf_scotus_opinions (row_index,col_index,tfidf_value) \
+    VALUES ({row_index},{col_index},{tfidf_value});".format(row_index=row,col_index=col,tfidf_value=scotus_tfidf_sparse_matrix[row,col]))
 
 #print the values in the table tfidf_scotus_opinions
 mycursor.execute("SELECT * FROM tfidf_scotus_opinions")
