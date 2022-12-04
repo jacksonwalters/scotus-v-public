60a406e564b1ba3a4460d48341c40b507c81b6f7
Jackson Walters <jacksonwalters@gmail.com>
2022-12-04 16:10:19 -0500

    load the full sparse matrix into sql

--- load_tfidf_into_sql.py
+++ load_tfidf_into_sql.py
@@ -15,8 +15,8 @@ mycursor = mydb.cursor()
 #insert sparse matrix in sql database
 scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
 rows,cols = scotus_tfidf_sparse_matrix.nonzero()
-for row,col in zip(rows[:100],cols[:100]):
-    print(row,col)
+for row,col in zip(rows,cols):
+    print(row,col,scotus_tfidf_sparse_matrix[row,col])
     mycursor.execute("INSERT INTO scvpo.tfidf_scotus_opinions (row_index,col_index,tfidf_value) \
     VALUES ({row_index},{col_index},{tfidf_value});".format(row_index=row,col_index=col,tfidf_value=scotus_tfidf_sparse_matrix[row,col]))
 
