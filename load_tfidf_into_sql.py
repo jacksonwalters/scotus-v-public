c775632caa120960ae4a6a687145704d0f2ff459
Jackson Walters <jacksonwalters@gmail.com>
2022-12-05 14:59:08 -0500

    proceed in batches of given size. 23mil entries so it's slow

--- load_tfidf_into_sql.py
+++ load_tfidf_into_sql.py
@@ -14,7 +14,16 @@ mycursor = mydb.cursor()
 #insert sparse matrix in sql database
 scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
 rows,cols = scotus_tfidf_sparse_matrix.nonzero()
-for row,col in zip(rows,cols):
+assert len(rows) == len(cols)
+BATCH_NUM = 0
+BATCH_SIZE = 1_000_000
+START = BATCH_NUM*BATCH_SIZE
+END = (BATCH_NUM+1)*BATCH_SIZE
+for row,col in zip(rows[START:END],cols[START:END]):
     print(row,col,scotus_tfidf_sparse_matrix[row,col])
     mycursor.execute("INSERT INTO scvpo.tfidf_scotus_opinions (row_index,col_index,tfidf_value) \
     VALUES ({row_index},{col_index},{tfidf_value});".format(row_index=row,col_index=col,tfidf_value=scotus_tfidf_sparse_matrix[row,col]))
+
+mydb.commit()
+mycursor.close()
+mydb.close()
