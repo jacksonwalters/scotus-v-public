ab4bf83c4efbb84e5b74ca8ad64491425339dbcb
Jackson Walters <jacksonwalters@gmail.com>
2022-12-05 17:01:17 -0500

    measure progress, upload in batches

--- load_tfidf_into_sql.py
+++ load_tfidf_into_sql.py
@@ -15,14 +15,16 @@ mycursor = mydb.cursor()
 scotus_tfidf_sparse_matrix = scipy.sparse.load_npz("./data/tf-idf/scotus_opinion/tfidf_matrix.npz")
 rows,cols = scotus_tfidf_sparse_matrix.nonzero()
 assert len(rows) == len(cols)
-BATCH_NUM = 0
+BATCH_NUM = 22
 BATCH_SIZE = 1_000_000
 START = BATCH_NUM*BATCH_SIZE
 END = (BATCH_NUM+1)*BATCH_SIZE
+count = 0
 for row,col in zip(rows[START:END],cols[START:END]):
-    print(row,col,scotus_tfidf_sparse_matrix[row,col])
+    if count%1_000 == 0: print(count/BATCH_SIZE)
     mycursor.execute("INSERT INTO scvpo.tfidf_scotus_opinions (row_index,col_index,tfidf_value) \
     VALUES ({row_index},{col_index},{tfidf_value});".format(row_index=row,col_index=col,tfidf_value=scotus_tfidf_sparse_matrix[row,col]))
+    count+=1
 
 mydb.commit()
 mycursor.close()
